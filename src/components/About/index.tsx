ce72e5917e0a10e7c457fe3bbff03393d8582516
William Grosset <williamhgrosset@gmail.com>
2019-09-29 17:20:43 -0700

    Update width of Window and height of page

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -4,14 +4,14 @@ import { TabBody, Anchor } from 'react95'
 const About: React.FC = () => (
   <TabBody>
     <p>
-      William Hamilton Grosset — 22 years old, dual citizen of the United
-      Kingdom and Canada, residing in Vancouver, and a Software Engineer working
-      with the Growth team at{' '}
-      <Anchor className="link" href="https://segment.com/">
+      William Grosset — 22 years old, dual citizen of the United Kingdom and
+      Canada, residing in Vancouver, and a Software Engineer working with the
+      Growth team at{' '}
+      <Anchor className="link" href="https://segment.com/" target="_blank">
         Segment
       </Anchor>
       . Previously, I worked with the Revenue team at{' '}
-      <Anchor className="link" href="https://www.change.org/">
+      <Anchor className="link" href="https://www.change.org/" target="_blank">
         Change.org
       </Anchor>
       . At 19, I worked with the LEAD Lab at the University of Victoria as a
