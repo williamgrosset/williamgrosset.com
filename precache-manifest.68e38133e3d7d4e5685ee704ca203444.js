3b7f5c66e5b09dd36ed657136f1325c05dc62bf3
William Grosset <williamhgrosset@gmail.com>
2019-09-29 23:21:03 -0700

    Updates

---
+++ precache-manifest.68e38133e3d7d4e5685ee704ca203444.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b2dbf9ef58a88c3f9e6f8192c5e217",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fbd61028ed89d08f8f3",
+    "url": "/static/css/main.0b5a59f2.chunk.css"
+  },
+  {
+    "revision": "e8713285a90645d9a487",
+    "url": "/static/js/2.4fbed312.chunk.js"
+  },
+  {
+    "revision": "8fbd61028ed89d08f8f3",
+    "url": "/static/js/main.019bbca3.chunk.js"
+  },
+  {
+    "revision": "dd8279c2f814c6cae2b4",
+    "url": "/static/js/runtime-main.7138ef54.js"
+  }
+]);
